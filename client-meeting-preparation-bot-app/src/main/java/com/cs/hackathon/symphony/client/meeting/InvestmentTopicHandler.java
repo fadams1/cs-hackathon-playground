0cc26ff89d4e903001bc5236a48e5649da80716b
fadams1 <fay737731@yahoo.co.uk>
2018-06-02 15:00:06 +0800

    [#1] legal id handler

--- client-meeting-preparation-bot-app/src/main/java/com/cs/hackathon/symphony/client/meeting/InvestmentTopicHandler.java
+++ client-meeting-preparation-bot-app/src/main/java/com/cs/hackathon/symphony/client/meeting/InvestmentTopicHandler.java
@@ -1,10 +1,11 @@
 package com.cs.hackathon.symphony.client.meeting;
 
+import nlp.model.Action;
 import org.symphonyoss.client.model.Chat;
 
 public class InvestmentTopicHandler implements TopicHandler {
     @Override
-    public TopicInformation collectTopicInformation(Chat rmChat) {
+    public TopicInformation collectTopicInformation(Action action, Chat rmChat) {
         return null;
     }
 
