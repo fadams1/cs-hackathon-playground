2f21219dba1674108ed234743973dd23ee04a884
wandatinkers <ianne.celino@gmail.com>
2018-06-04 15:14:22 +0800

    initial integration with camunda (disabled by default)

---
+++ client-meeting-preparation-bot-app/src/main/java/com/cs/hackathon/symphony/ExternalTaskClientBuilder.java
@@ -0,0 +1,33 @@
+package com.cs.hackathon.symphony;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.task.ExternalTask;
+
+import java.util.function.Function;
+
+public class ExternalTaskClientBuilder {
+
+    public static final String WFENGINE_REST = "http://localhost:8080/engine-rest";
+
+    public ExternalTaskClient getNewExternalTaskClient(final boolean enabled){
+        if (enabled){
+            ExternalTaskClient client=ExternalTaskClient.create()
+            .baseUrl(WFENGINE_REST)
+            .build();
+            return client;
+        }
+        return null;
+    }
+
+    public void subscribeToTopic(final String topicName, ExternalTaskClient client, Function<ExternalTask, ExternalTask> callback) {
+        // subscribe to an external task topic as specified in the process
+        client.subscribe(topicName)
+        .lockDuration(1000) // the default lock duration is 20 seconds, but you can override this
+        .handler((externalTask, externalTaskService) -> {
+            callback.apply(externalTask);
+            // Complete the task
+            externalTaskService.complete(externalTask);
+        })
+        .open();
+    }
+}
--- client-meeting-preparation-bot-app/src/main/java/com/cs/hackathon/symphony/client/meeting/ClientMeetingMain.java
+++ client-meeting-preparation-bot-app/src/main/java/com/cs/hackathon/symphony/client/meeting/ClientMeetingMain.java
@@ -1,17 +1,46 @@
 package com.cs.hackathon.symphony.client.meeting;
 
+import com.cs.hackathon.symphony.ExternalTaskClientBuilder;
 import com.cs.hackathon.symphony.SymphonyClientBuilder;
+import com.cs.hackathon.symphony.ThrowingFunction;
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.symphonyoss.client.exceptions.AuthenticationException;
 import org.symphonyoss.client.exceptions.InitException;
 
 import java.time.LocalDateTime;
 
 public class ClientMeetingMain {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientMeetingController.class);
+    private static final boolean WFENGINE_CONNECTED = false;
+    private static final String TOPIC_INITIATE_RM_CONVERSATION = "topic-initiate-rm-conversation";
 
     public static void main(String[] args) throws InitException, AuthenticationException {
+        if (WFENGINE_CONNECTED) {
+            LOGGER.info("Connecting to workflow engine..");
+            ExternalTaskClientBuilder clientBuilder = new ExternalTaskClientBuilder();
+            ExternalTaskClient client = clientBuilder.getNewExternalTaskClient(WFENGINE_CONNECTED);
+            clientBuilder.subscribeToTopic(TOPIC_INITIATE_RM_CONVERSATION,
+                            client,
+                            startProcessorCallback());
+        } else {
+            startProcessor();
+        }
+    }
+
+    private static ThrowingFunction<ExternalTask, ExternalTask> startProcessorCallback() {
+        return ExternalTask -> {
+            startProcessor();
+            return ExternalTask;
+        };
+    }
+
+    private static void startProcessor() throws InitException, AuthenticationException {
         new ClientMeetingController(new SymphonyClientBuilder()).notifyClientMeeting(
-                new ClientMeetingEvent("fay.adams@credit-suisse.com",
-                        "fay", LocalDateTime.now().plusDays(1))
+            new ClientMeetingEvent("fay.adams@credit-suisse.com",
+             "fay", LocalDateTime.now().plusDays(1))
         );
     }
 }
